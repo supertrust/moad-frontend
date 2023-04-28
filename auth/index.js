e0901831ae411c13831155265a76cfac71b8397c
fahadtufail007 <t.fahad@mufiin.co.kr>
2023-04-28 12:42:30 +0500

    IC-156 feat: add protected route

---
+++ auth/index.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+export const ProtectRoute = ({ children }) => {
+  const router = useRouter();
+  const [loading, setloading] = useState(true);
+  const publicRoutes = ['/', '/login/', '/signup/'];
+
+  const parseJwt = (token) => {
+    try {
+      return JSON.parse(atob(token.split('.')[1]));
+    } catch (e) {
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const decodedJwt = parseJwt(token);
+    const isTokenValid = decodedJwt && decodedJwt?.exp * 1000 > Date.now();
+    const isAuthRoute = publicRoutes.includes(router.asPath);
+
+    if (isAuthRoute && isTokenValid) {
+      router.replace('/');
+    } else if (!isTokenValid) {
+      router.replace('/login/');
+    }
+    setTimeout(() => setloading(false), 500);
+  }, []);
+
+  return loading ? <></> : <>{children}</>;
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { store } from '../store/store';
+import { ProtectRoute } from 'auth';
+import { Provider as StoreProvider } from 'react-redux';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ProtectRoute>
+      <StoreProvider store={store}>
+        <Component {...pageProps} />
+      </StoreProvider>
+    </ProtectRoute>
+  );
 }

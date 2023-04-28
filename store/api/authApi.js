f6352e61a3ad747549cddf0e0891ef4a0a636f4d
fahadtufail007 <t.fahad@mufiin.co.kr>
2023-04-28 12:43:11 +0500

    IC-156 feat: add redux store and auth reducer

---
+++ store/api/authApi.js
@@ -0,0 +1,13 @@
+import api from 'services/api';
+
+export async function loginApi(data) {
+  const res = await api.post('user/login', data);
+  localStorage.setItem('token', res.data.accessToken);
+  api.defaults.headers.Authorization = `Bearer ${res.data.accessToken}`;
+  return res.data.data;
+}
+
+export async function signupApi(data) {
+  const res = await api.post('user/signup', data);
+  return res.data.data;
+}
---
+++ store/slices/authSlice.js
@@ -0,0 +1,37 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { loginApi, signupApi } from '../api/authApi';
+
+const initialState = {
+  user: {},
+};
+
+export const login = createAsyncThunk('auth/login', async (data) => {
+  const response = await loginApi(data);
+  return response.data;
+});
+
+export const signup = createAsyncThunk('auth/signup', async (data) => {
+  const response = await signupApi(data);
+  return response.data;
+});
+
+export const logout = () => {
+  localStorage.clear();
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [login.fulfilled]: (state, action) => {
+      state.user = action.payload;
+    },
+  },
+});
+
+export const {} = authSlice.actions;
+
+export const selectUser = (state) => state.auth.user;
+
+export default authSlice.reducer;

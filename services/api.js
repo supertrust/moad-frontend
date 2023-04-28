2390de8b17e656515f2d9f817348903e71b2fc23
fahadtufail007 <t.fahad@mufiin.co.kr>
2023-04-28 12:41:46 +0500

    IC-156 feat: add base url in config file

---
+++ services/api.js
@@ -0,0 +1,23 @@
+import Axios from 'axios';
+
+const api = Axios.create({
+  baseURL: process.env.API_BASE_URL,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
+
+// api.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const errorCode = error?.response?.status;
+//     if (errorCode === 401) {
+//       localStorage.clear();
+//       window.location.href = '/authentication/sign-in/';
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+export default api;

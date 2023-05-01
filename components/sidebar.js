56e3062d1bc38587f82917d72607115e7939138d
jubairmufin <k.jubair@mufin.co.kr>
2023-05-01 19:06:30 +0530

    IC-160-UI-for-Side-Menu

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,11 +1,51 @@
+import Link from "next/link";
+// import { useState } from "react";
+
 function Sidebar() {
   return (
-    <aside
-      id='aside-bar'
-      className='flex flex-col py-8 space-y-8 bg-blue-500 shadow-sm w-full md:w-60'
-    >
-      This is the sidebar
-    </aside>
+    <div className="content">
+      <h1 className="side-logo">
+        <img src="/images/logo-pc.svg" />
+      </h1>
+      <div className="sidemenu-wrap">
+        <ul className="menu-wrap">
+          <li className="menu-list active">
+            <div className="link">
+              <i class="icon">
+                <img src="/images/ic-dashboard-active.png" />
+              </i>
+              <div className="name"><Link href="/Advertisement" >Advertising management</Link></div>
+            </div>
+          </li>
+          <li className="menu-list">
+            <div className="link">
+              <i class="icon"><img src="/images/ic-statistics.png" /></i>
+              <div className="name"><Link href="/Statics">Statics</Link></div>
+            </div>
+            <ul className="sub-wrap"></ul>
+          </li>
+          <li className="menu-list">
+            <div className="link">
+              <i class="icon"><img src="/images/ic-mypage.png" /></i>
+              <div className="name">My Page</div>
+            </div>
+            <ul className="sub-wrap"></ul>
+          </li>
+          <li className="menu-list">
+            <div className="link">
+              <i class="icon"><img src="/images/ic-inquire.png" /></i>
+              <div className="name">Customer Service Center</div>
+            </div>
+            <ul className="sub-wrap"></ul>
+          </li>
+        </ul>
+      </div>
+      <div className="side-logout">
+        <div className="logout-btn">
+          <div className="text">log out</div>
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ pages/Statics.js
@@ -0,0 +1,18 @@
+export default function Statics() {
+  return (
+    <div className="statics">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis esse
+      consequatur nulla tempora temporibus quos ratione explicabo ex optio,
+      incidunt qui, quam dicta voluptatibus sunt obcaecati, amet saepe?
+      Recusandae unde maiores cumque accusantium, magnam impedit quis deleniti
+      obcaecati totam rem consequatur fugiat. Accusantium, unde harum, alias
+      culpa dolorum incidunt nemo asperiores ex rem veritatis iste mollitia
+      libero corrupti, excepturi odio natus quis voluptates fugiat pariatur ab
+      architecto nostrum tempora ipsam! Quasi, debitis corporis! Ipsam nihil
+      deleniti nisi provident fuga asperiores voluptatibus incidunt quis est
+      vero fugit nam ducimus expedita reprehenderit doloribus quas, quam esse
+      repellat adipisci fugiat. Maxime, nostrum natus! <br />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit error maxime nisi expedita veritatis officia eveniet similique, quae animi architecto, pariatur fuga quia praesentium maiores quis eius rerum rem, aut tenetur sequi saepe cumque ducimus quos! Tempore laboriosam, necessitatibus vitae excepturi cumque neque pariatur debitis veritatis temporibus esse? Quis, recusandae.
+    </div>
+  );
+}

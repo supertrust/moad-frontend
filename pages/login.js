eb859a7a24e8a5c5442fbdf99ed906235597de44
rizki-mufin <m.rizki@mufin.co.kr>
2023-05-03 17:02:47 +0700

    fix broken login page

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,9 @@
 import LoginModulePage from "@/modules/Auth/Pages/LoginModulePage";
 
 export default function Login() {
-  return <main className='min-h-screen p-10'><LoginModulePage /></main>;
+  return (
+    <main className="min-h-screen">
+      <LoginModulePage />
+    </main>
+  );
 }

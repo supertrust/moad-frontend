5bcc9627ba5e41a7e00503178e9218b734d5787f
hussainHanifM <h.hussain@mufin.co.kr>
2023-05-03 07:07:06 +0500

    logout functional & layout dir replace

--- modules/Layout/Components/footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <div id="inner_footer" className="inner-footer">
-       <span>2023@copyright. All rights reserved</span>
-    </div>
-  );
-}
-
-export default Footer;

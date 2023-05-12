c4043ace2fa6827b63d75c761acfdb310a2d0128
rizki-mufin <m.rizki@mufin.co.kr>
2023-05-12 21:22:45 +0700

    dashboard page

--- src/sections/dashboard/AdList/AdList.tsx
+++ src/sections/dashboard/AdList/AdList.tsx
@@ -45,7 +45,7 @@ export default function AdListModule() {
                 delete
               </p>
             </button>
-            <div>
+            <div className="select-box only-pc">
               <Form.Select aria-label="Default select example">
                 <option>Choose your campaign type</option>
                 <option value="1">One</option>
@@ -72,32 +72,29 @@ export default function AdListModule() {
                 />
               </div>
               <div className={styles.grid}>
+                <div className={`${styles.typeWrap} ${styles.gridBox}`}>ad name</div>
                 <div className={styles.gridBox}>ad name</div>
                 <div className={styles.gridBox}>ad name</div>
                 <div className={styles.gridBox}>ad name</div>
-                <div className={styles.gridBox}>ad name</div>
-                <div className={styles.gridBox}>ad name</div>
+                <div className={`${styles.statusWrap} ${styles.gridBox}`}>ad name</div>
               </div>
             </div>
-            <div className={`${styles.listHd} ${styles.listFlex}`}>
-              <div className={styles.chkBox}>
-                <input
-                  type="checkbox"
-                  name="all_chk"
-                  id="all_chk"
-                  className="all-chk"
-                  value=""
-                />
-              </div>
-              <div className={styles.grid}>
-                <div className={styles.gridBox}>ad name</div>
-                <div className={styles.gridBox}>
-                  Celebrating the opening of the Icarus service. 25 characters
-                </div>
-                <div className={styles.gridBox}>ad name</div>
-                <div className={styles.gridBox}>ad name</div>
-                <div className={styles.gridBox}>ad name</div>
-              </div>
+            <div className="tab-content all-wrap on">
+              <ul className="list-wrap">
+                <li className={styles.listFlex}>
+                 <div className={styles.chkBox}>
+                  <input type="checkbox" className="list-chk" name="list_chk" />
+                    </div>
+                      <a href="ad-detail-list" className={styles.grid}>
+                        <div className={`${styles.typeWrap} ${styles.gridBox}`}>고정형</div>
+                        <div className={styles.gridBox}>이카루스 서비스 오픈 출시 기념입니다 25글자</div>
+                        <div className={styles.gridBox}>1/120</div>
+                        <div className={styles.gridBox}>2023. 01. 01 ~ 2023. 03. 01</div>
+                        <div className={`${styles.statusWrap} ${styles.gridBox}`}>광고검수중</div>
+                        <i className="only-mb ic-arrow-right"></i>
+                      </a>
+                </li>
+              </ul>
             </div>
           </div>
       </div>

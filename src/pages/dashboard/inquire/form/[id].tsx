bd5487ea8f0eb5fd8381ce22bed75779b249b64e
Nabeel-Mufin <a.nabeel@mufin.co.kr>
2023-05-26 18:24:37 +0500

    "IC-277" #fix the inquiry page, only admin now update the inquiry answer

--- src/pages/dashboard/inquire/form/[id].tsx
+++ src/pages/dashboard/inquire/form/[id].tsx
@@ -1,9 +1,29 @@
-import { useRouter } from "next/router"
-import Index from "./index";
+import { useRouter } from "next/router";
+import EditInquiryAnswer from "./index";
+import { useGetUserRole } from "@src/apis/user";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "@src/contexts/AuthContext";
 
 export default function Form() {
   const router = useRouter();
+  const [isAuthenticated, setisAuthenticated] = useState(false);
+  const authValue = useContext(AuthContext);
+  useEffect(() => {
+    if (authValue) {
+      setisAuthenticated(authValue.isAuthenticated);
+    }
+  }, []);
+
+  const { data } = useGetUserRole({ isAuthenticated });
   return (
-    <Index id={router.query.id as string} />
-  )
+    <>
+      {data?.user_role === "admin" ? (
+        <EditInquiryAnswer id={router.query.id as string} />
+      ) : (
+        <div className="flex items-center justify-center h-full">
+          <h1 className='font-semibold'>Only admin is allowed to update data</h1>
+        </div>
+      )}
+    </>
+  );
 }

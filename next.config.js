620373cce1095b59399920b4ffb481c19f235423
Nabeel-Mufin <a.nabeel@mufin.co.kr>
2023-05-29 10:57:51 +0500

    "IC-270" #add hostname as remotePattern

--- next.config.js
+++ next.config.js
@@ -1,18 +1,16 @@
+const hostnames = ["upload.wikimedia.org", "dev-icarus.mufin.lol"];
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
   env: {
     API_BASE_URL: process.env.API_URL,
-    KOKAO_MAP_URL:process.env.KOKAO_MAP_URL
+    KOKAO_MAP_URL: process.env.KOKAO_MAP_URL,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-        hostname: 'dev-icarus.mufin.lol',
-      },
-    ],
+    remotePatterns: hostnames.map((hostname) => ({
+      protocol: "https",
+      hostname,
+    })),
   },
 };
 

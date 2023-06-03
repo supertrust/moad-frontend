7d90bb2aa1b312b7b6799c0e2377f79abd72c986
Nabeel-Mufin <a.nabeel@mufin.co.kr>
2023-06-03 13:23:59 +0500

    "IC-316" #add layout in cargo dashboard

--- src/pages/cargo-dashboard/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CargoOwnerDashboardScreen = () => {
-  return <main className="p-10">Hey Cargo Owner!, You are login</main>;
-};
-
-export default CargoOwnerDashboardScreen;
---
+++ src/pages/dashboard/cargo/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+const CargoOwnerDashboardScreen = () => {
+  return (
+    <h4 className="flex items-center justify-center min-h-full">
+      Hey Cargo Owner!, You are login
+    </h4>
+  );
+};
+
+export default CargoOwnerDashboardScreen;

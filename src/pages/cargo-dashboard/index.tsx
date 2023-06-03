ef93739bea2fd66b5244ecfe783aaaeadb93e57a
Nabeel-Mufin <a.nabeel@mufin.co.kr>
2023-06-03 12:42:25 +0500

    "IC-316" #implement cargo owner login api and add redirection

---
+++ src/pages/cargo-dashboard/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CargoOwnerDashboardScreen = () => {
+  return <main className="p-10">Hey Cargo Owner!, You are login</main>;
+};
+
+export default CargoOwnerDashboardScreen;

ece53d8e042886036189a03b4e637f6a97b2e661
shahneel-mufin <126235038+shahneel-mufin@users.noreply.github.com>
2023-09-16 13:58:45 +0500

    Ic 450 implementation of html css to react for landing page (#201)
    * IC-450 Implementation of landing page on react
    
    * DNA-450 Implement mobile version
    
    * IC-450 fix text and alignment issues
    
    * IC-450 mobile nav bar issues
    
    * IC-450 merge code

--- src/components/pages/Landing/index.tsx
+++ src/components/pages/Landing/index.tsx
@@ -1,58 +1,100 @@
 import React from "react";
 
-import logo from "../../../../public/images/landing/header/logo-pc.svg"
-import logoMb from "../../../../public/images/landing/header/logo-mb.svg"
-import sec1Img from "../../../../public/images/landing/main/img-section-1.svg"
+import logo from "../../../../public/images/landing/header/logo-pc.svg";
+import logoMb from "../../../../public/images/landing/header/logo-mb.svg";
+import sec1Img from "../../../../public/images/landing/main/img-section-1.svg";
+import mbImg1 from "../../../../public/images/landing/main/bg-section-1-mb.png";
+import {  useState } from 'react';
 
 import Image from "next/image";
+import "animate.css";
+import "animate.css/animate.min.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 const Landing = () => {
-  return (
-    <div id="landing_">
-      <header id="header" className="header">
-        <div className="header-continer">
-          <h1 className="logo">
-            <a href="#" className="logo-pc">
-              {/* <img src="../../../../public/images/landing/header/logo-pc.svg" alt=""  /> */}
-              <Image src={logo} alt="" className="_img" />
+  const [isShowMobileNav , setIsShowMobileNav] = useState<Boolean>(false);
+  const navClassName = isShowMobileNav ? '_nav active' : '_nav';
 
+  const toggleMobileNav=()=>{
+    setIsShowMobileNav(!isShowMobileNav)
+  }
+  return (
+    <div id='landing_'>
+      <header
+        id='header'
+        className='header'>
+        <div className='header-continer'>
+          <h1 className='logo'>
+            <a
+              href='#'
+              className='logo-pc'>
+              <Image
+                src={logo}
+                alt=''
+                className='_img'
+              />
             </a>
-            <a href="#" className="logo-mb">
-              <Image src={logoMb} alt="" />
+            <a
+              href='#'
+              className='logo-mb'>
+              <Image
+                src={logoMb}
+                alt=''
+              />
             </a>
           </h1>
-          <button className="menu-mb-btn only-mb" id="menu_mb_btn"></button>
-          <nav id="nav" className="nav">
-            <div className="menu-mb-top only-mb">
-              <h1 className="menu-logo">
-                <a href="#" className="logo-mb">
-                    <Image src={logoMb} alt="" />
+          <button className='menu-mb-btn only-mb' id='menu_mb_btn' onClick={toggleMobileNav}></button>
+          <nav
+            id='_nav'
+            className={navClassName}>
+            <div className='menu-mb-top only-mb'>
+              <h1 className='menu-logo'>
+                <a
+                  href='#'
+                  className='logo-mb'>
+                  <Image
+                    src={logoMb}
+                    alt=''
+                  />
                 </a>
               </h1>
-              <button className="menu-close-btn" id="menu_close_btn"></button>
+              <button
+                className='menu-close-btn'
+                id='menu_close_btn' onClick={toggleMobileNav}></button>
             </div>
-            <ul className="menu">
-              <li className="menu-pl-list main active">
-                <a href="main" className="menu-link">
+            <ul className='menu'>
+              <li className='menu-list main active'>
+                <a
+                  href='#'
+                  className='menu-link'>
                   서비스소개
                 </a>
               </li>
-              <li className="menu-pl-list about-us">
-                <a href="about-us" className="menu-link">
+              <li className='menu-list about-us'>
+                <a
+                  href='#'
+                  className='menu-link'>
                   회사소개
                 </a>
               </li>
-              <li className="menu-pl-list inquire">
-                <a href="inquire" className="menu-link">
+              <li className='menu-list inquire'>
+                <a
+                  href='#'
+                  className='menu-link'>
                   문의하기
                 </a>
               </li>
             </ul>
-            <div className="menu-terms-wrap only-mb">
-              <a href="#" className="pl-text">
+            <div className='menu-terms-wrap only-mb'>
+              <a
+                href='#'
+                className='_text'>
                 이용약관
               </a>
-              <a href="#" className="pl-text">
+              <a
+                href='#'
+                className='_text'>
                 개인정보처리방침
               </a>
             </div>
@@ -60,180 +102,219 @@ const Landing = () => {
         </div>
       </header>
 
-      <main id="mian">
-        <section className="section01">
-          <div className="section01-container">
-            <div className="container">
-              <div className="title">
-                <strong>이카루스</strong>
-                움직이는 광고판,
+      <main id='mian'>
+        <section className='section01'>
+          <div className='section01-container'>
+            <div className='container'>
+              <div className='_title'>
+                <strong className='pc-point'>이카루스</strong>
+                <br />
+                <strong className='mb-point'>움직이는 광고판,</strong>
                 <br />
                 전국광고의 시작
               </div>
-              <a href="inquire" className="inquire-link">
+              <a
+                href='inquire'
+                className='inquire-link'>
                 서비스 문의하기
               </a>
             </div>
-            <div className="section01-img">
-              <Image src={sec1Img} alt="" className="img" />
+            <div className='section01-img'>
+              <Image
+                src={sec1Img}
+                alt=''
+                className='img'
+              />
+            </div>
+            <div className='section01-mb-bg'>
+              {/* <img src="./images/main/bg-section-1-mb.png" alt=""> */}
+              <Image
+                src={mbImg1}
+                alt='image'
+              />
             </div>
           </div>
         </section>
-        <section className="section02">
-          <div className="pl-text">최고를 지향하며 사명감과 신념으로</div>
-          <div className="pl-text">
-            <p className="pl-text pl-text-black">차량광고</p>의 선두주자로 여러분의
-            곁에 서고자 하는
+        <section className='section02'>
+          <div className='_text'>
+            최고를 지향하며 사명감과 신념으로 <br />
+            <p className='_text text-black'>차량광고</p>의 선두주자로 여러분의
+            곁에
+            <br className='only-mb' /> 서고자 하는 <br className='only-pc' />
+            <p className='_text text-black'>우리는</p>{" "}
+            <p className='_text text-blue'>이카루스</p>
+            <p className='_text text-black'>입니다.</p>
           </div>
-          <div className="pl-text pl-text-black">
-            우리는 <p className="pl-text pl-text-blue">이카루스</p>입니다.
-          </div>
-          <div className="sub-pl-text">
-            시인성, 이동성, 경제성 모든 것을 만족시키고, 효율과 능률적인 면에서
-            최고를 지향합니다.
+          <div className='_sub-text'>
+            시인성, 이동성, 경제성 모든 것을 만족시키고,
+            <br className='only-mb' /> 효율과 능률적인 면에서 최고를 지향합니다.
           </div>
         </section>
-        <section className="section03">
-          <div className="container">
-          <div className="pl-text">
-                    움직이는 광고판,<br />
-                    <strong className="pl-text-2">차량 랩핑광고를 소개합니다.</strong> 
-                </div>
-            <ul className="pl-list-wrap">
-              <li className="pl-list">
-                <div className="pl-list-title">기업의 가치와 품위</div>
-                <div className="pl-list-pl-text">
+        <section className='section03'>
+          <div className='container'>
+            <div className='_text'>
+              움직이는 광고판,
+              <br />
+              <strong className='_text'>차량 랩핑광고</strong>를 소개합니다.
+            </div>
+            <ul className='_list-wrap'>
+              <li className='_list'>
+                <div className='list-title'>기업의 가치와 품위</div>
+                <div className='list-text'>
                   브랜드 광고를 통한 기업의 가치와 품위를 높여줍니다.
                 </div>
               </li>
-              <li className="pl-list">
-                <div className="pl-list-title">좋은 시인성과 이동성</div>
-                <div className="pl-list-pl-text">
+              <li className='_list'>
+                <div className='list-title'>좋은 시인성과 이동성</div>
+                <div className='list-text'>
                   뛰어난 시인성으로 고객을 사로잡아 강하게 인식시킵니다.
                 </div>
               </li>
-              <li className="pl-list">
-                <div className="pl-list-title">적은 비용으로 최대의 효과</div>
-                <div className="pl-list-pl-text">
+              <li className='_list'>
+                <div className='list-title'>적은 비용으로 최대의 효과</div>
+                <div className='list-text'>
                   면적 대비 저렴한 비용과 뛰어난 노출 효과로 최대의 효율을
                   발휘합니다.
                 </div>
               </li>
             </ul>
-            <div className="mb-slide">
-              <ul className="pl-list-wrap">
-                <li className="pl-list">
-                  <div className="pl-list-num">01</div>
-                  <div className="pl-list-title">기업의 가치와 품위</div>
-                  <div className="pl-list-pl-text">
-                    브랜드 광고를 통한 기업의 가치와 품위를 높여줍니다.
+            <div
+              className='swiper mb-slide'
+              id='mb_slide'>
+              <Swiper
+                className='list-wrap-mb swiper-wrapper'
+                spaceBetween={10}
+                slidesPerView={1.1}
+                // onSlideChange={() => console.log("slide change")}
+                // onSwiper={(swiper) => console.log(swiper)}
+              >
+                <SwiperSlide className="_list swiper-slide'">
+                  <div className='list-num'>01</div>
+                  <div className='list-title'>기업의 가치와 품위</div>
+                  <div className='list-text'>
+                  브랜드 광고를 통한 기업의 가치와
+                  품위를 높여줍니다.          
                   </div>
-                </li>
-                <li className="pl-list">
-                  <div className="pl-list-num">02</div>
-                  <div className="pl-list-title">좋은 시인성과 이동성</div>
-                  <div className="pl-list-pl-text">
-                    뛰어난 시인성으로 고객을 사로잡아 강하게 인식시킵니다.
+                </SwiperSlide>
+
+                <SwiperSlide className="_list swiper-slide'">
+                  <div className='list-num'>02</div>
+                  <div className='list-title'> 좋은 시인성과 이동성</div>
+                  <div className='list-text'>
+                  뛰어난 시인성으로 고객을 사로잡아
+                  강하게 인식시킵니다.                    
                   </div>
-                </li>
-                <li className="pl-list">
-                  <div className="pl-list-num">03</div>
-                  <div className="pl-list-title">적은 비용으로 최대의 효과</div>
-                  <div className="pl-list-pl-text">
-                    면적 대비 저렴한 비용과 뛰어난 노출 효과로 최대의 효율을
-                    발휘합니다.
+                </SwiperSlide>
+
+                <SwiperSlide className="_list swiper-slide'">
+                  <div className='list-num'>03</div>
+                  <div className='list-title'>적은 비용으로 최대의 효과</div>
+                  <div className='list-text'>
+                  면적 대비 저렴한 비용과 뛰어난 노출 효과로 최대의
+                  효율을 발휘합니다.
                   </div>
-                </li>
-              </ul>
+                </SwiperSlide>
+              </Swiper>
             </div>
           </div>
         </section>
-        <section className="section04">
-          <div className="container">
-            <div className="title-wrap">
-              <div className="title">
+        <section className='section04'>
+          <div className='container'>
+            <div className='_title-wrap'>
+              <div className='_title'>
                 화물 랩핑광고
-                <br />
+                <br className='only-pc' />
                 어디서 어떻게
-                <br className="only-pc" />
+                <br />
                 시작해야 할까요?
               </div>
-              <div className="sub-pl-text">
-                원하는 차량 선택부터 광고리포트까지
-                <br />
-                모두 이카루스에서!
-              </div>
-              <div className="pl-text">
+              <div className='_sub-text'>
                 원하는 차량 선택부터 광고리포트까지
                 <br />
                 모두 이카루스에서!
               </div>
             </div>
-            <ul className="pl-list-wrap">
-              <li className="pl-list">
-                <div className="pl-text-wrap">
-                  <div className="pl-text">
-                    내가 원하는 차량을 직접 선택할 수 있으며,
+            <ul className='_list-wrap'>
+              <li className='_list'>
+                <div className='_text-wrap'>
+                  <div className='_text'>
+                    내가 원하는 차량을 직접 선택할 수{" "}
+                    <br className='d-none-desktop' />
+                    있으며,
+                    <br className='d-none-mobile' />
+                    차량과의{" "}
+                    <strong className='_text'>계약부터 정산 진행까지</strong>
                     <br />
-                    
-                    <strong className="pl-text">차량과의 계약부터 정산 진행까지</strong>
-                    <strong className="pl-text">이카루스에서 도와드립니다.</strong>
+                    <strong className='_text'>
+                      이카루스에서 도와드립니다.
+                    </strong>
                   </div>
                 </div>
               </li>
-              <li className="pl-list">
-                <div className="pl-text-wrap">
-                  <div className="pl-text">
-                    디자인 시안이 없어도 OK, 걱정하지 마세요!
-                    <br />
-                    직종에 맞는 최상의 디자인을 제작해드립니다.
+              <li className='_list'>
+                <div className='_text-wrap'>
+                  <div className='_text'>
+                    <strong className='_text'>
+                      디자인 시안이 없어도 OK, 걱정하지
+                      <br className='.d-none-desktop' />
+                      마세요!
+                    </strong>
+                    <br className='d-none-mobile' />
+                    직종에 맞는 최상의 디자인을 <br className="d-none-desktop"/> 제작해드립니다.
                   </div>
                 </div>
               </li>
-              <li className="pl-list">
-                <div className="pl-text-wrap">
-                  <div className="pl-text">
-                    실시간 광고 현황은 물론
+              <li className='_list'>
+                <div className='_text-wrap'>
+                  <div className='_text'>
+                    실시간 <strong className='_text'>광고 현황</strong>은 물론
                     <br />
-                    광고 리포트까지 확인하실 수 있어요.
+                    <strong className='_text'>광고 리포트까지 확인</strong>하실
+                    수 있어요.
                   </div>
                 </div>
               </li>
             </ul>
           </div>
         </section>
-        <section className="section05">
-          <div className="container">
-            <div className="title">
+        <section className='section05'>
+          <div className='container'>
+            <div className='_title'>
               차량광고 중개 플랫폼
               <br />
               이카루스와 함께 하세요
             </div>
-            <div className="btns">
-              <a href="" className="btn google-store">
-                <i className="icon"></i>
-                <div className="link-pl-text">
-                  <div className="pl-text">화물주 전용앱</div>
-                  <div className="down-pl-text">
-                    다운로드 <span className="down-pl-text"></span>
+            <div className='btns'>
+              <a
+                href=''
+                className='btn google-store'>
+                <i className='icon'></i>
+                <div className='link-text'>
+                  <div className='_text'>화물주 전용앱</div>
+                  <div className='down-text'>
+                    다운로드 <span className='down-text'></span>
                   </div>
                 </div>
               </a>
-              <a href="" className="btn app-store">
-                <i className="icon"></i>
-                <div className="link-pl-text">
-                  <div className="pl-text">화물주 전용앱</div>
-                  <div className="down-pl-text">
-                    다운로드 <span className="down-pl-text"></span>
+              <a
+                href=''
+                className='btn app-store'>
+                <i className='icon'></i>
+                <div className='link-text'>
+                  <div className='_text'>화물주 전용앱</div>
+                  <div className='down-text'>
+                    다운로드 <span className='down-text'></span>
                   </div>
                 </div>
               </a>
-              <a href="" className="btn system">
-                <i className="icon"></i>
-                <div className="link-pl-text">
-                  <div className="pl-text">광고주 시스템</div>
-                  <div className="down-pl-text">
-                    다운로드 <span className="down-pl-text"></span>
+              <a
+                href=''
+                className='btn system'>
+                <i className='icon'></i>
+                <div className='link-text'>
+                  <div className='_text'>광고주 시스템</div>
+                  <div className='down-text'>
+                    다운로드 <span className='down-text'></span>
                   </div>
                 </div>
               </a>
@@ -242,37 +323,41 @@ const Landing = () => {
         </section>
       </main>
 
-      <footer id="footer">
-        <div className="container">
-          <div className="terms-line">
-            <ul className="pl-list-wrap">
-              <li className="pl-list">
-                <a href="" className="link">
+      <footer id='footer'>
+        <div className='container'>
+          <div className='terms-line'>
+            <ul className='_list-wrap'>
+              <li className='_list'>
+                <a
+                  href=''
+                  className='link'>
                   이용약관
                 </a>
               </li>
-              <li className="pl-list">
-                <a href="" className="link">
+              <li className='_list'>
+                <a
+                  href=''
+                  className='link'>
                   개인정보처리방침
                 </a>
               </li>
             </ul>
           </div>
-          <div className="company-line">
-            <span className="pl-text">주식회사 애드메타</span>
-            <span className="bar">ㅣ</span>
-            <span className="pl-text">대표자 : 이진희</span>
+          <div className='company-line'>
+            <span className='_text'>주식회사 애드메타</span>
+            <span className='bar'>ㅣ</span>
+            <span className='_text'>대표자 : 이진희</span>
           </div>
-          <div className="company-num">
-            <span className="pl-text">
+          <div className='company-num'>
+            <span className='_text'>
               세종특별자치시 남세종로 454, 8층 802호 에이864호 (보람동,
               강남제일타워)
             </span>
-            <span className="bar">ㅣ</span>
-            <span className="pl-text">사업자등록번호 559-87-02646</span>
-            <span className="bar">ㅣ</span>
+            <span className='bar'>ㅣ</span>
+            <span className='_text'>사업자등록번호 559-87-02646</span>
+            <span className='bar'>ㅣ</span>
           </div>
-          <div className="copyright pl-text">
+          <div className='copyright _text'>
             Copyright 2022 ICARUS All rights reserved.
           </div>
         </div>

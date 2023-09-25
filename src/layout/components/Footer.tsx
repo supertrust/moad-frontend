8e7d804d6620e4ea7188eae73bdee0102310af31
shahneel-mufin <126235038+shahneel-mufin@users.noreply.github.com>
2023-09-25 16:02:47 +0500

    Ic 413 guide tweeks (#230)
    * IC-413 Guide Detail Pages and fixes
    
    -Wrapping Adv Added (mobile & desktop)
    -Confirm screen Added (mobile & desktop )
    -Process screen Added  (mobile & desktop)
    
    * IC-413 Remove footer from confirm page

--- src/layout/components/Footer.tsx
+++ src/layout/components/Footer.tsx
@@ -1,7 +1,22 @@
 function Footer() {
+  // Get the current URL pathname
+  const currentURL = window.location.pathname;
+
+  // Define the URL to hide the footer on
+  const targetURL = '/dashboard/customer-service/guide/confirm';
+
+  // Check if the current URL pathname matches the target URL
+  const shouldHideFooter = currentURL === targetURL;
+
+  // Conditionally render the footer based on the URL
+  if (shouldHideFooter) {
+    return null; // Hide the footer
+  }
+
+  // Render the footer for other pages
   return (
-    <div id="inner_footer" className="inner-footer" style={{ width : "100%" }}>
-       <span>2023@copyright. All rights reserved</span>
+    <div id="inner_footer" className="inner-footer" style={{ width: '100%' }}>
+      <span>2023@copyright. All rights reserved</span>
     </div>
   );
 }

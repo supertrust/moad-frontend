6f6efadb57633eceda0e7569cc3713cba6fb2b62
gavishs123 <90839966+gavishs123@users.noreply.github.com>
2023-11-17 10:32:09 +0530

    Fix select issue (#321)
    * fix login page design
    
    * fix signup page design
    
    * fix signup page design
    
    * pull from main
    
    * fix dashboard design
    
    * fix dashboard design
    
    * fix dashboard design
    
    * change arrow icon
    
    * change arrow icon
    
    * add pagination functionality in dashboard
    
    * fix notice page design
    
    * fix guide and faq page design
    
    * fix guide and faq page design
    
    * fix guide and faq page design
    
    * fix guide and faq page design
    
    * fix guide, terms and faq page design
    
    * fix guide, terms and faq page design
    
    * inquiry form
    
    * inquiry form
    
    * inquiry form design
    
    * inquiry form design
    
    * statistics page design
    
    * statistics page design
    
    * dashboard page design
    
    * make save advertisement model design responsive
    
    * make save advertisement model design responsive
    
    * make save advertisement model design responsive
    
    * make save advertisement model design responsive
    
    * make dashboard inner pages design
    
    * make dashboard inner pages design
    
    * notification page design
    
    * notification page design
    
    * notification page design
    
    * match FAQ page design
    
    * vehicle page design
    
    * Terms page design
    
    * Cargo dashboard page design
    
    * Fix distance issue
    
    * Fix distance issue
    
    * Fix time issue
    
    * move cargo pages
    
    * move cargo pages
    
    * fix cargo location page design
    
    * cargo dashboard page design
    
    * add new field in register page
    
    * Create member detail page
    
    * Create member detail page
    
    * Advertisment detail popup
    
    * Pull from main
    
    * modification in ad popup
    
    * Modifications
    
    * Modifications
    
    * Modifications in statistics page
    
    * Modifications in save popup
    
    * Modifications
    
    * Statistic page modifications
    
    * View popup add in detail page
    
    * Modifications in save popup model

--- src/sections/dashboard/AdList/AdList.tsx
+++ src/sections/dashboard/AdList/AdList.tsx
@@ -189,7 +189,7 @@ export default function AdListModule() {
 							<Form.Select
 								onChange={(e) => setType(e.target.value as AdTypesType)}
 								aria-label='Default select example'
-								className='font-medium adlist  border-1 border-[#2F48D1] text-[#2F48D1] h-[36px]'>
+								className='font-medium custom-select  border-1 border-[#2F48D1] text-[#2F48D1] h-[36px]'>
 								<option value=''>광고 유형 선택</option>
 								<option value='fixed_ad'>고정형</option>
 								<option value='national_ad'>전국형</option>

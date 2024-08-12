4e8ffbf5fa94f6ebc5c611098130e6bacd8957be
MD. Fahim Ahmed Sajib <61555992+sfahim-mufin@users.noreply.github.com>
2024-08-12 10:07:15 +0600

    [IC-1755] 🐛 Bug Resolved: Sign Up Button is Not Working  (#516)
    * fix signup issue
    
    * code reformat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,11 @@ import Loader from "@src/components/Loader";
 import { AuthProvider } from '@src/contexts/AuthContext';
 import { ConfirmDialogProvider } from '@src/contexts/ConfirmDialogContext';
 import Layout from '@src/layout';
-
 import ReactQueryClient from '@src/services/ReactQueryClient';
 import '@src/styles/globals.css';
 import '@src/styles/responsive.css';
 import { setRouter } from '@src/utils/axios';
+import { WithoutAuthenticationRoute } from "@src/utils/route";
 import type { AppProps } from 'next/app';
 import { Noto_Sans_KR } from 'next/font/google';
 import { useRouter } from 'next/router';
@@ -16,61 +16,59 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'rsuite/dist/rsuite.css';
 
-
 const notoSansKR = Noto_Sans_KR({
-	weight: '400',
-	subsets: ['latin'],
+    weight: '400',
+    subsets: ['latin'],
 });
 
 const _App = ({ Component, pageProps }: AppProps) => {
-	const router = useRouter();
-	const { pathname:path } = router;
-	const token = typeof window !== 'undefined'? localStorage.getItem("token") : null;
+    const router = useRouter();
+    const { pathname: path } = router;
+    const token = typeof window !== 'undefined' ? localStorage.getItem("token") : null;
 
-	useEffect(()=>{
-		setRouter(router)
-	},[router])
+    useEffect(() => {
+        setRouter(router)
+    }, [router])
 
-	useEffect(() => {
-		if (!token && !path.includes("/login")) {
-			const returnUrl = window.location.href.replace(window.location.origin, '');
-			if(returnUrl.length && returnUrl!=="/")router.replace(`/login?returnUrl=${returnUrl}`);
-			else router.replace(`/login`);
-		}
-		else if(path==="/" && token)
-			router.replace(`/dashboard`);
+    useEffect(() => {
+        if (path.includes('/signup') && !token)
+            return;
+        else if (!token && !path.includes("/login")) {
+            const returnUrl = window.location.href.replace(window.location.origin, '');
+            if (returnUrl.length && returnUrl !== "/") router.replace(`/login?returnUrl=${returnUrl}`);
+            else router.replace(`/login`);
+        } else if (path === "/" && token)
+            router.replace(`/dashboard`);
 
-	}, [path, router]);
+    }, [path, router]);
 
-	if (path.includes('/dashboard') && !path.includes("/login") && token) {
-		return (
-			<main className={notoSansKR.className}>
-				<Layout>
-					<Component {...pageProps} />
-				</Layout>
-			</main>
-		);
-	}
+    if (path.includes('/dashboard') && !path.includes("/login") && token) {
+        return (
+            <main className={notoSansKR.className}>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </main>
+        );
+    } else if (WithoutAuthenticationRoute.every(substring => !path.includes(substring)))
+        return <div className={'flex justify-center pt-5'}><Loader size={'lg'}/></div>
 
-	else if(!path.includes("/login"))
-		return <div className={'flex justify-center pt-5'}><Loader size={'lg'}/></div>
-
-	return (
-		<main className={notoSansKR.className}>
-			<Component {...pageProps} />
-		</main>
-	);
+    return (
+        <main className={notoSansKR.className}>
+            <Component {...pageProps} />
+        </main>
+    );
 };
 
 export default function App(props: AppProps) {
-	return (
-		<ReactQueryClient>
-			<AuthProvider>
-					<ConfirmDialogProvider>
-						<_App {...props} />
-					</ConfirmDialogProvider>
-			</AuthProvider>
-			<ToastContainer />
-		</ReactQueryClient>
-	);
+    return (
+        <ReactQueryClient>
+            <AuthProvider>
+                <ConfirmDialogProvider>
+                    <_App {...props} />
+                </ConfirmDialogProvider>
+            </AuthProvider>
+            <ToastContainer/>
+        </ReactQueryClient>
+    );
 }

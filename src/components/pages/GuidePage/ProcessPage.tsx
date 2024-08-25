cf71198a5dd4fd90f543d89366cb5becbd271cda
MD. Fahim Ahmed Sajib <61555992+sfahim-mufin@users.noreply.github.com>
2024-08-25 20:31:01 +0600

    [IC-1826] 👑 Improvement: The Right Depth and Directions of Guide Pages (#541)
    * pc , confirm and process update
    
    * guide page issue fixed
    
    * guide page file structure fixed
    
    * fixed the route name
    
    * sidebar routing issue fixed
    
    * guide page issue fixed
    
    * fixed the guide wrapping page
    
    * fixed responsive issue
    
    * fixed font sizes
    
    * background size issue resolved
    
    * fixed the bold issue
    
    * removed step 5
    
    * flex shrink added

--- src/components/pages/GuidePage/ProcessPage.tsx
+++ src/components/pages/GuidePage/ProcessPage.tsx
@@ -5,77 +5,124 @@ import { useRouter } from "next/router";
 import React from "react";
 import Head from "next/head";
 import Image from "next/image";
-import useAuth from "@src/hooks/useAuth";
-
-
 function ProcessPage() {
   const router = useRouter();
-  const { dictionary, setLang, lang } = useAuth();
-
   const onBack = () => {
     router.back();
   };
-
   return (
-    <>
-      <Head>
-        <title>{dictionary.process_page.titles}</title>
-      </Head>
-      <div style={{ backgroundColor: "#f5f7fb !important" }}>
-        <div className={"only-mb"}>
-          <div className={`${styles["mobile-top-header"]}`}>
-            <div style={{ alignSelf: "flex-start" }}>
-              <ArrowBack
-                className={"ml-4"}
-                handleAction={onBack}
-              />
-            </div>
-            <div className={styles["header"]}>{dictionary.process_page.headers}</div>
-            <div></div>
-          </div>
-        </div>
-
-        <div
-          className={`pl-[45px] pr-[32px]  pt-[20px] pb-[35px] text-gray-700 flex flex-col gap-[20px] ${styles["_ava_root"]}`}
-          style={{ backgroundColor: "#f5f7fb" }}>
-          <div style={{ backgroundColor: "white" }}>
-            <div className={`${styles["bg_Head_"]}`}>
-              <div className='font-bold text-[30px] text-[#373737]'>{dictionary.process_page.headers}</div>
-              <div className='font-bold text-[40px] text-[#373737]'>
-                {dictionary.process_page.titles}
+      <>
+        <Head>
+          <title>이카루스 광고주</title>
+        </Head>
+        <div style={{ backgroundColor: "#f5f7fb !important" }}>
+          <div className={"only-mb"}>
+            <div className={`${styles["mobile-top-header"]}`}>
+              <div style={{ alignSelf: "flex-start" }}>
+                <ArrowBack
+                    className={"ml-4"}
+                    handleAction={onBack}
+                />
               </div>
+              <div className={styles["header"]}>가이드</div>
+              <div></div>
             </div>
-            <Card
-              variant='elevation'
-              elevation={3}
-              className={`flex flex-col justify-between h-full gap-2 !shadow-[0px_2px_8px_0px_rgba(38,51,77,0.00)] ${styles["_bottom_card_list_wrapper"]}`}>
-              <div className={`${styles["_root__list_items"]}`}>
-                {dictionary.process_page.steps.map((step, index) => (
-                  <div key={index} className={`${styles["_root__text_img"]}`}>
+          </div>
+          <div
+              className={`pl-[45px] pr-[32px]  pt-[20px] pb-[35px] text-gray-700 flex flex-col gap-[20px] ${styles["_ava_root"]}`}
+              style={{ backgroundColor: "#f5f7fb" }}>
+            <div style={{ backgroundColor: "white" }}>
+              <div className={`${styles["bg_Head_"]}`}>
+                <div className='font-bold text-[30px] text-[#373737]'>가이드</div>
+                <div className='font-bold text-[40px] text-[#373737]'>
+                  이카루스 광고 진행 프로세스
+                </div>
+              </div>
+              <Card
+                  variant='elevation'
+                  elevation={3}
+                  className={`flex flex-col justify-between h-full gap-2 !shadow-[0px_2px_8px_0px_rgba(38,51,77,0.00)] ${styles["_bottom_card_list_wrapper"]}`}>
+                <div className={`${styles["_root__list_items"]}`}>
+                  <div className={`${styles["_root__text_img"]}`}>
                     <div className={`${styles["_step_avatar_style"]}`}>
                       <Image
-                        src={`/images/process/${index + 1}.svg`}
-                        alt={`step${index + 1}`}
-                        width={110}
-                        height={100}
+                          src='/images/process/1.svg'
+                          alt='step1'
+                          width={110}
+                          height={100}
                       />
                     </div>
                     <div className={`${styles["text_ss"]}`}>
-                      <h3>{step.number}</h3>
-                      <h4>{step.title}</h4>
+                      <h3>STEP 1</h3>
+                      <h4>광고 신청하기</h4>
                       <p>
-                        {step.description}
+                        광고 목적에 따라 광고 유형, 희망 기간, 희망 차
+                        <br className={`${styles["br_mobile"]}`} />량 등
+                        <br className={`${styles["br_desktop"]}`} />
+                        조건 을 설정하여 광고를 신청합니다.
                       </p>
                     </div>
                   </div>
-                ))}
-              </div>
-            </Card>
+                  <div className={`${styles["_root__text_img"]}`}>
+                    <div className={`${styles["_step_avatar_style"]}`}>
+                      <Image
+                          src='/images/process/2.svg'
+                          alt='step2'
+                          width={110}
+                          height={100}
+                      />
+                    </div>
+                    <div className={`${styles["text_ss"]}`}>
+                      <h3>STEP 2</h3>
+                      <h4>광고 상담</h4>
+                      <p>
+                        이카루스 담당자가 확인 후 연락드립니다. <br />
+                        광고 진행에 관한 자세한 상담을 도와드립니다.
+                      </p>
+                    </div>
+                  </div>
+                  <div className={`${styles["_root__text_img"]}`}>
+                    <div className={`${styles["_step_avatar_style"]}`}>
+                      <Image
+                          src='/images/process/3.svg'
+                          alt='step3'
+                          width={110}
+                          height={100}
+                      />
+                    </div>
+                    <div className={`${styles["text_ss"]}`}>
+                      <h3>STEP 3</h3>
+                      <h4>차량 모집</h4>
+                      <p>
+                        이카루스에서 검증된 차량들이며, <br />
+                        해당 광고를 부착할 차량을 모집합니다.
+                      </p>
+                    </div>
+                  </div>
+                  <div className={`${styles["_root__text_img"]}`}>
+                    <div className={`${styles["_step_avatar_style"]}`}>
+                      <Image
+                          src='/images/process/4.svg'
+                          alt='step4'
+                          width={110}
+                          height={100}
+                      />
+                    </div>
+                    <div className={`${styles["text_ss"]}`}>
+                      <h3>STEP 4</h3>
+                      <h4>광고 진행</h4>
+                      <p>
+                        운행차량의 실시간 위치 등 <br />
+                        광고 진행을 확인하실 수 있습니다.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Card>
+            </div>
           </div>
         </div>
-      </div>
-    </>
+      </>
   );
 }
-
 export default ProcessPage;
